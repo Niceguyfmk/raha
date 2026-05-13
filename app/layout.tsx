768c6d0a1142d2998e12d22142754654659112a4
unknown <faizmdkhan007@gmail.com>
2026-05-13 11:45:15 +0530

    added GTM code to layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -15,6 +16,27 @@ export default function RootLayout({
     <html lang="en">
       <body className="antialiased">
         {children}
+
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-X36J2WF3Y0"
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+
+            function gtag() {
+              dataLayer.push(arguments);
+            }
+
+            window.gtag = gtag;
+
+            gtag('js', new Date());
+            gtag('config', 'G-X36J2WF3Y0');
+          `}
+        </Script>
       </body>
     </html>
   );
